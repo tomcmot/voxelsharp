d0c304fb467811b59d02b4415ff9a13782cf1e07
Tom Carstens <tomcarstens@gmail.com>
2026-04-29 11:12:53 -0500

    remove unnecessary tests

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -3,19 +3,6 @@
 open System
 open Xunit
 open Engine
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
-
-[<Fact>]
-let ``plane is a plane`` () =
-    let expected = Array.init (16*16*16) (fun i ->
-        if i &&& 0b000011110000 = 0 then Chunk.Block.Box else Chunk.Block.Air
-    )
-    Assert.Equal(expected.Length, Chunk.plane.Length)
-    Assert.Equivalent(expected, Chunk.plane)
-    for i = 0 to Chunk.plane.Length-1 do
-        Assert.True(expected[i] = Chunk.plane[i], sprintf "At %i, expected %A actual %A\n" i expected[i] Chunk.plane[i])
 
 let ``GetVoxel tests`` () =
     Assert.Equal(Chunk.Block.Box, Chunk.getVoxel Chunk.plane 0 0 0)
